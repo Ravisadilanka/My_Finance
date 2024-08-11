00c8ae3a215cdc4fb8a20a2e150f85645562bdda
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-08-11 20:34:33 +0530

    Responsive Email page

--- src/pages/Email.jsx
+++ src/pages/Email.jsx
@@ -6,6 +6,7 @@ import document from "../assets/document.jpg";
 import { IoMdArrowDropdown } from "react-icons/io";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { FaBars } from "react-icons/fa";
 
 const Email = () => {
   const [message, setMessage] = useState("");
@@ -58,13 +59,27 @@ const Email = () => {
     "image",
     "video",
   ];
+  const [isSidemenuOpen, setIsSidemenuOpen] = useState(true);
+
+  function toggleSidemenu() {
+    setIsSidemenuOpen(!isSidemenuOpen);
+  }
 
   return (
     <div className="flex flex-col h-screen">
-      <Header />
-      <div className="flex h-screen">
-        <Sidemenu />
-        <div className="flex flex-col w-full h-full bg-lightBlue p-5 gap-5">
+      <div className="md:block flex items-center justify-between px-5">
+        <div className="block md:hidden">
+          <button onClick={toggleSidemenu}>
+            <FaBars className="text-md" />
+          </button>
+        </div>
+        <Header />
+      </div>
+      <div className="md:flex">
+        <div className={isSidemenuOpen ? "hidden md:block" : ""}>
+          <Sidemenu />
+        </div>
+        <div className={isSidemenuOpen ? "flex flex-col w-full h-full bg-lightBlue p-5 gap-5" : "hidden md:block"}>
           <div className="bg-white flex justify-between p-5 rounded-xl items-center">
             <h1 className="text-lg font-bold">Bulk Email</h1>
             <button className="flex gap-3 items-center border-2 border-lightBlue py-1 px-2 rounded-lg">
@@ -82,7 +97,7 @@ const Email = () => {
             <div className="flex flex-col w-full">
               <form
                 action="submit"
-                className="flex flex-col w-full gap-7 h-full"
+                className="flex flex-col w-full gap-4 md:gap-7 h-full"
               >
                 <div className="relative w-full">
                   <IoMdArrowDropdown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
@@ -99,8 +114,8 @@ const Email = () => {
                   placeholder="Subject"
                   className="w-full p-3 rounded-xl border-2 border-gray-500"
                 />
-                <div className="relative flex flex-col h-full pb-10 ">
-                  <h3 className="absolute top-0 left-1 p-2 text-sm text-buttonBlue z-10">
+                <div className="md:relative md:flex md:flex-col md:h-full md:pb-10 ">
+                  <h3 className="md:absolute md:top-0 md:left-1 md:p-2 md:text-sm text-buttonBlue md:z-10">
                     Message
                   </h3>
                   <ReactQuill
@@ -108,7 +123,7 @@ const Email = () => {
                     onChange={setMessage}
                     modules={modules}
                     formats={formats}
-                    className="h-full pt-5"
+                    className="md:h-full pt-5"
                   />
                 </div>
               </form>
